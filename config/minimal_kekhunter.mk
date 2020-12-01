8020d7987e8ed5b8261ab2b62c1dc0b7c60327c8
Hilledkinged <kailing2@live.com>
2020-12-01 21:37:39 +0200

    Added some parts of kek, WIP still.

---
+++ config/minimal_kekhunter.mk
@@ -0,0 +1,23 @@
+# Minimal list of packages for nh functionality
+#
+# This minimal variant is mainly for devices with low space of /system
+
+# Apps
+PRODUCT_PACKAGES += \
+	kekhunter \
+	kekterm
+
+# Binaries
+PRODUCT_PACKAGES += \
+	bootkali \
+	bootkali_bash \
+	bootkali_env \
+	bootkali_init \
+	bootkali_log \
+	bootkali_login \
+	busybox_nh \
+	chrootmgr \
+	hid-keyboard \
+	killkali \
+	setHID \
+	usbarmory
---
+++ config/nogoogle.mk
@@ -0,0 +1,12 @@
+# Packages
+PRODUCT_PACKAGES += \
+	AuroraServices \
+	AuroraStore \
+	GmsCore \
+	FakeStore \
+	NetworkLocation \
+	UnifiedNlp \
+	GsfProxy \
+	privapp-permissions-microg.xml \
+	microg-power-whitelist.xml \
+	privapp-permissions-auroraservices.xml
