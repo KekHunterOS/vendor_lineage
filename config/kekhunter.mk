df953076bb68687c6df43f241d0cb2db1c06cb99
Hilledkinged <kailing2@live.com>
2020-12-03 20:24:49 +0200

    Some changes that still need testing

--- config/kekhunter.mk
+++ config/kekhunter.mk
@@ -1,1 +1,34 @@
-RECOVERY_VARIANT := twrp
+# Usual list of packages for nh functionality
+#
+# This minimal variant is mainly for devices with low space of /system
+
+# Apps
+PRODUCT_PACKAGES += \
+	kekhunter \
+	kekterm \
+	kex
+
+# NHStore stufff
+PRODUCT_PACKAGES += \
+	nhstore \
+	nhsotrepriv \
+	nhstoreprivperms
+
+# Binaries NH
+PRODUCT_PACKAGES += \
+	bootkali \
+	bootkali_bash \
+	bootkali_env \
+	bootkali_init \
+	bootkali_log \
+	bootkali_login \
+	busybox_nh \
+	chrootmgr \
+	hid-keyboard \
+	killkali \
+	setHID \
+	usbarmory
+
+# Binaries 3party
+PRODUCT_PACKAGES += \
+	
